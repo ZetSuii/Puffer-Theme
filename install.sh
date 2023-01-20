96094c98ec826b347eabba6c586c83aab8bbe6fe
ZetSuii <123192964+ZetSuii@users.noreply.github.com>
2023-01-20 19:08:18 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+SCRIPT_VERSION="V1.0"
+
+clear
+echo
+#print_brake 70
+echo "* PufferPanel Theme Script @ $SCRIPT_VERSION"
+echo
+echo "* Copyright (C) 2023."
+echo
+echo "* This script is not associated with the official PufferPanel."
+#print_brake 70
+echo
+echo "1. Install Simple Dark"
+echo "7. Close / Cancel"
+read -p "Please enter a number: " choice
+if [ $choice == "1" ]
+    then
+    bash <(curl -s https://raw.githubusercontent.com/Sigma-Production/PteroFreeStuffinstaller/V1.10.1/resources/Addons.sh)
+if [ $choice == "2" ]
+    then
+    echo "Canceling..."
+    exit 0
+fi
