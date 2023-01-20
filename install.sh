adad635bc3fe877f04b2b349f48fda9790dead22
ZetSuii <123192964+ZetSuii@users.noreply.github.com>
2023-01-20 19:14:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,11 +10,13 @@ echo "* PufferPanel Theme Script @ $SCRIPT_VERSION"
 echo
 echo "* Copyright (C) 2023."
 echo
+echo "Do not worry ! More are coming very soon!"
+echo
 echo "* This script is not associated with the official PufferPanel."
 #print_brake 70
 echo
 echo "1. Install Simple Dark"
-echo "7. Close / Cancel"
+echo "2. Close / Cancel"
 read -p "Please enter a number: " choice
 if [ $choice == "1" ]
     then
