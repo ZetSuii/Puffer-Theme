2ec55783f4407550ffa5c7984916ad2d588e5430
ZetSuii <123192964+ZetSuii@users.noreply.github.com>
2023-01-21 15:47:52 +0100

    Update en-install.sh

--- en-install.sh
+++ en-install.sh
@@ -21,7 +21,7 @@ read -p "Please enter a number: " choice
 if [ $choice == "1" ]
     then
     cd /var/www/pufferpanel/theme
-    bash <(git@github.com:ZetSuii/Puffer-Theme.git)
+    wget <(git@github.com:ZetSuii/Puffer-Theme.git)
     fi
 if [ $choice == "2" ]
     then
